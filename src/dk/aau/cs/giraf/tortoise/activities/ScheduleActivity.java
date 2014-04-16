6e7849ba28ff3d6ae3a08bfcd8b670d5e216340f
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-04-16 14:56:13 +0200

    added schedule activity for week plan

---
+++ src/dk/aau/cs/giraf/tortoise/activities/ScheduleActivity.java
@@ -0,0 +1,11 @@
+package dk.aau.cs.giraf.tortoise.activities;
+
+import android.os.Bundle;
+
+public class ScheduleActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+    }
+}
