26a3234ab00aad60bd5d78e999f4d6d956411764
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-04-22 09:58:09 +0200

    made intent for schedule activity work

--- src/dk/aau/cs/giraf/tortoise/activities/ScheduleActivity.java
+++ src/dk/aau/cs/giraf/tortoise/activities/ScheduleActivity.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import dk.aau.cs.giraf.tortoise.R;
 import dk.aau.cs.giraf.tortoise.helpers.GuiHelper;
 
 public class ScheduleActivity extends Activity
 {
     protected void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_activity);
+
         // Get intent, action and MIME type
         Intent intent = getIntent();
-        GuiHelper.ShowToast(this,"God påske!");
+
         if (intent.getExtras() == null)
         {
             GuiHelper.ShowToast(this, "Ingen data i intent");
