d3d9644c6d666ab3bd7537af6f176103c13b1833
djeppe12@student.aau.dk <3012811941>
2014-04-23 15:54:08 +0200

    More DBController

--- src/dk/aau/cs/giraf/tortoise/controller/DBController.java
+++ src/dk/aau/cs/giraf/tortoise/controller/DBController.java
@@ -1,11 +1,16 @@
 package dk.aau.cs.giraf.tortoise.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by David on 28-03-14.
  */
 public class DBController {
 
-    // Singleton pattern
+    /*********************
+     * Singleton pattern *
+     *********************/
     private static DBController instance;
 
     private DBController(){};
@@ -19,5 +24,62 @@ public class DBController {
             dbController = new DBController();
         }
         return dbController;
+    }
+
+    /*******************************
+     * Class variable declarations *
+     *******************************/
+    private boolean success;
+
+    /***********************
+     * SequenceType ENUMS: *
+     ***********************
+     * SEQUENCE = 0        *
+     * SCHEDULE = 1        *
+     * STORY = 2           *
+     * PARROT = 3          *
+     ***********************/
+
+    /******************
+     * Public methods *
+     ******************/
+
+    /**
+     * Save Sequence.
+     *
+     * @param seq
+     * @param sequenceType
+     * @param citizenID
+     * @return boolean
+     */
+    public boolean saveSequence(Sequence seq, int sequenceType, int citizenID){
+        success = false;
+        // This is where the magic should happen
+        return success;
+    }
+
+    /**
+     * Load specific Sequence.
+     *
+     * @param id
+     * @return Sequence
+     */
+    public Sequence loadSequence(int id){
+        Sequence seq = new Sequence();
+        // This is where the magic should happen
+        return seq;
+    }
+
+    /**
+     * Returns all sequences of the defined type on the defined citizen.
+     *
+     * @param citizenID
+     * @param sequenceType
+     * @return ArrayList of Sequences
+     */
+    public ArrayList loadCurrentProfileSequences(int citizenID, int sequenceType){
+        ArrayList sequences = new ArrayList();
+        // This is where the magic should happen
+        return sequences;
     }
 }
