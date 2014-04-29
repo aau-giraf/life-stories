3a0697dba1c0f624b0bc306c9483055426874642
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-04-29 13:59:38 +0200

    pictograms can now be chosen through week schedule editor.

--- src/dk/aau/cs/giraf/tortoise/activities/ScheduleActivity.java
+++ src/dk/aau/cs/giraf/tortoise/activities/ScheduleActivity.java
@@ -1,5 +1,22 @@
 package dk.aau.cs.giraf.tortoise.activities;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.view.View;
+
+import dk.aau.cs.giraf.tortoise.helpers.LifeStory;
+
 public class ScheduleActivity extends TortoiseActivity{
     //TODO move common methods here
+
+    public void startPictosearch(View v)
+    {
+        Intent i = new Intent();
+        i.setComponent(new ComponentName("dk.aau.cs.giraf.pictosearch", "dk.aau.cs.giraf.pictosearch.PictoAdminMain"));
+        i.putExtra("purpose", "single");
+        i.putExtra("currentChildID", LifeStory.getInstance().getChild().getId());
+        i.putExtra("currentGuardianID", LifeStory.getInstance().getGuardian().getId());
+
+        this.startActivityForResult(i, 2);
+    }
 }
--- src/dk/aau/cs/giraf/tortoise/activities/ScheduleViewActivity.java
+++ src/dk/aau/cs/giraf/tortoise/activities/ScheduleViewActivity.java
@@ -217,15 +217,4 @@ public class ScheduleViewActivity extends ScheduleActivity
             }
         }
     }
-
-    public void startPictosearch(View v)
-    {
-        Intent i = new Intent();
-        i.setComponent(new ComponentName("dk.aau.cs.giraf.pictosearch", "dk.aau.cs.giraf.pictosearch.PictoAdminMain"));
-        i.putExtra("purpose", "single");
-        i.putExtra("currentChildID", LifeStory.getInstance().getChild().getId());
-        i.putExtra("currentGuardianID", LifeStory.getInstance().getGuardian().getId());
-
-        this.startActivityForResult(i, 2);
-    }
 }
