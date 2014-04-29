c3d210e01c8413796b3f5800ef068005ffaf471f
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2014-04-29 10:22:10 +0200

    made weekday buttons smaller in edit. Layout now scales in portrait and landscape mode.

--- src/dk/aau/cs/giraf/tortoise/activities/ScheduleEditActivity.java
+++ src/dk/aau/cs/giraf/tortoise/activities/ScheduleEditActivity.java
@@ -3,7 +3,9 @@ package dk.aau.cs.giraf.tortoise.activities;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +16,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import dk.aau.cs.giraf.gui.GToggleButton;
 import dk.aau.cs.giraf.pictogram.Pictogram;
 import dk.aau.cs.giraf.tortoise.R;
 import dk.aau.cs.giraf.tortoise.controller.Sequence;
@@ -21,11 +24,24 @@ import dk.aau.cs.giraf.tortoise.helpers.GuiHelper;
 
 public class ScheduleEditActivity extends ScheduleActivity
 {
+    Boolean deviceInPortraitMode = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.schedule_edit_activity);
+
+        // check whether tablet is in portrait or landscape mode and set the layout accordingly
+        // landscape mode shows mode days than portrait mode
+        int screenOrientation = getResources().getConfiguration().orientation;
+        if(screenOrientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            setContentView(R.layout.schedule_edit_activity);
+        }
+        else
+        {
+            setContentView(R.layout.schedule_edit_activity_portrait);
+        }
 
         // Get intent, action and MIME type
         Intent intent = getIntent();
@@ -35,20 +51,13 @@ public class ScheduleEditActivity extends ScheduleActivity
             GuiHelper.ShowToast(this, "Ingen data modtaget fra Tortoise");
             finish();
         }
+    }
 
-        //ListHandler lh = new ListHandler();
-        //lh.ilmerscorner();
-
-        /*
-        setContentView(R.layout.schedule_edit_activity);
-
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);*/
-
+    public void weekdaySelected(View v)
+    {
+        // pushing a toggle button has no effect
+        GToggleButton btn = (GToggleButton) findViewById(v.getId());
+        btn.setToggled(true);
     }
 
     public List<Sequence> getPictograms()
@@ -184,44 +193,5 @@ public class ScheduleEditActivity extends ScheduleActivity
 
 
         return pictogramSequence;
-    }
-}
-
-class ListHandler extends ScheduleEditActivity
-{
-    private ArrayAdapter<String> listAdapter;
-
-    public void ilmerscorner()
-    {
-        GuiHelper.ShowToast(this, "..");
-        try{
-            ListView lv = (ListView) findViewById(R.id.listView);
-
-            // Create and populate a List of planet names.
-            String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                    "Jupiter", "Saturn", "Uranus", "Neptune"};
-
-            ArrayList<String> planetList = new ArrayList<String>();
-            planetList.addAll(Arrays.asList(planets));
-
-
-            // Create ArrayAdapter using the planet list.
-            listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
-
-            // Add more planets. If you passed a String[] instead of a List<String>
-            // into the ArrayAdapter constructor, you must not add more items.
-            // Otherwise an exception will occur.
-            listAdapter.add( "Ceres" );
-            listAdapter.add( "Pluto" );
-            listAdapter.add( "Haumea" );
-            listAdapter.add( "Makemake" );
-            listAdapter.add( "Eris" );
-
-            // Set the ArrayAdapter as the ListView's adapter.
-            lv.setAdapter( listAdapter );
-        } catch (Exception ex)
-        {
-            GuiHelper.ShowToast(this, ex.toString());
-        }
     }
 }
