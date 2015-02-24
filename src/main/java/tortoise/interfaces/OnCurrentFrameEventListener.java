4be94c61a483c966ed891b05e36b3a36925f80cb
David H. Junker <djunke12@student.aau.dk>
2015-02-24 10:56:28 +0100

    fixed src java path

--- src/main/java/tortoise/interfaces/OnCurrentFrameEventListener.java
+++
@@ -1,7 +0,0 @@
-package dk.aau.cs.giraf.tortoise.interfaces;
-
-import dk.aau.cs.giraf.tortoise.activities.EditModeFrameView;
-
-public interface OnCurrentFrameEventListener {
-	public void OnCurrentFrameChanged(EditModeFrameView editModeFrameView, int ChoiceNumber);
-}
--- src/main/java/tortoise/interfaces/OnMediaFrameEventListener.java
+++
@@ -1,7 +0,0 @@
-package dk.aau.cs.giraf.tortoise.interfaces;
-
-import dk.aau.cs.giraf.tortoise.controller.MediaFrame;
-
-public interface OnMediaFrameEventListener {
-	public void OnMediaFrameChanged(MediaFrame mediaFrame);
-}
